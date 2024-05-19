48620f8e9763ae24849f0eb58c69dbd9ae822a03
olumayor99 <olumayoor99@gmail.com>
2024-05-19 22:36:03 +0300

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -29,9 +29,9 @@ resource "aws_launch_template" "ec2_website" {
 
   user_data = base64encode(<<-EOF
             #!/bin/bash
-            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
             sudo apt-get update
-            sudo apt-get install ca-certificates curl -y
+            sudo apt-get install -y ca-certificates curl
             sudo install -m 0755 -d /etc/apt/keyrings
             sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -41,7 +41,7 @@ resource "aws_launch_template" "ec2_website" {
             $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
             sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
             sudo apt-get update
-            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+            sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             sudo docker run -d -p 80:80 --name website olumayor99/doyenify-devops:latest
 
               EOF

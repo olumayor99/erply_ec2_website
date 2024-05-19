34d05917507c3685854af02aec3aee688aa37136
olumayor99 <olumayoor99@gmail.com>
2024-05-19 20:50:36 +0300

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,3 @@
-# Get Availability Zones in Region
-data "aws_availability_zones" "available" {}
-
-locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
-}
-
 # Get Ubuntu AMI in Region
 data "aws_ami" "ubuntu" {
 
@@ -22,55 +15,6 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"]
 }
-
-# Create VPC
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
-
-  name = "${var.prefix}-vpc"
-  cidr = var.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
-
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  create_egress_only_igw = true
-}
-
-# Create Security Group
-resource "aws_security_group" "ec2_website" {
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # Create Launch Configuration
 resource "aws_launch_configuration" "ec2_website" {
   name            = "ec2-websitelaunch-configuration"
@@ -96,7 +40,7 @@ resource "aws_launch_configuration" "ec2_website" {
                 sudo apt-get update
                 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
                 docker run -d -p 80:80 --name ec2_website olumayor99/doyenify-devops:latest
-                
+
               EOF
 }
 
@@ -106,7 +50,7 @@ resource "aws_autoscaling_group" "ec2_website" {
   min_size             = 1
   max_size             = 3
   desired_capacity     = 2
-  vpc_zone_identifier  = module.vpc.public_subnets
+  vpc_zone_identifier  = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   tag {
     key                 = "Environment"
@@ -119,31 +63,85 @@ resource "aws_autoscaling_group" "ec2_website" {
   }
 }
 
-# Create Elastic Load Balancer
-resource "aws_elb" "ec2_website" {
-  name               = "ec2-website-load-balancer"
-  availability_zones = module.vpc.azs
-  security_groups    = [aws_security_group.ec2_website.id]
+resource "aws_vpc" "ec2_website" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    lb_protocol       = "HTTP"
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.ec2_website.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.ec2_website.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "ec2_website" {
+  vpc_id = aws_vpc.ec2_website.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "ec2_website" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_http.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "ec2_website" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ec2_website.id
+
   health_check {
-    target              = "HTTP:80/"
+    path                = "/"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
+    matcher             = "200-299"
   }
 }
 
-# ELB attachment to Autoscaling Group
-# Create a new load balancer attachment
+resource "aws_lb_listener" "ec2_website" {
+  load_balancer_arn = aws_lb.ec2_website.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ec2_website.arn
+  }
+}
+
 resource "aws_autoscaling_attachment" "ec2_website" {
   autoscaling_group_name = aws_autoscaling_group.ec2_website.id
-  elb                    = aws_elb.ec2_website.id
+  lb_target_group_arn    = aws_lb_target_group.ec2_website.arn
 }

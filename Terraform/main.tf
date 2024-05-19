0fb50286999651b4a0268ca0841e55b5051a84d2
olumayor99 <olumayoor99@gmail.com>
2024-05-19 22:15:09 +0300

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -28,20 +28,25 @@ resource "aws_launch_template" "ec2_website" {
   }
 
   user_data = base64encode(<<-EOF
-              #!/bin/bash
-                sudo apt-get update
-                sudo apt-get install ca-certificates curl
-                sudo install -m 0755 -d /etc/apt/keyrings
-                sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-                sudo chmod a+r /etc/apt/keyrings/docker.asc
-                echo \
-                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                sudo apt-get update
-                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-                docker run -d -p 80:80 --name ec2_website olumayor99/doyenify-devops:latest
+            #!/bin/bash
+            sudo apt-get remove docker docker-engine docker.io
+            sudo apt-get update -qq
+            sudo apt-get install -y \
+                apt-transport-https \
+                ca-certificates \
+                curl \
+                software-properties-common
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+            sudo add-apt-repository \
+            "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) \
+            stable"
+
+            sudo apt-get update
+            sudo apt-get install docker-ce -y
+            docker run -d -p 80:80 --name website olumayor99/doyenify-devops:latest
+            
               EOF
   )
 
